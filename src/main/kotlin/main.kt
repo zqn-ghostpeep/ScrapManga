2c07c16233643a5d4336756782dcf2a1c478357d
zqn-ghostpeep <julianpause@hotmail.com>
2024-03-29 19:50:39 -0400

    stomach bubbly, im struggling

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+//just some examples on how this will be used. use the object any way se want. In good intentions
 fun main() {
-    println("Hello World!")
+    var testCase = mangaDexScrap()
 }
--- src/main/kotlin/mangaDexScrap.kt
+++ src/main/kotlin/mangaDexScrap.kt
@@ -2,6 +2,8 @@
 //on this first episode we are we'll be pulling from manga dex
 
 class mangaDexScrap {
+    //where all the searched stuff goes
+    var theSearchedList: ArrayList<mangaObj> = ArrayList<mangaObj>()
     //a list of manga to choose from when the search result is pulled
     fun searchedMangas(searchWord: String) {
 
